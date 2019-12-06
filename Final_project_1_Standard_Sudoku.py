d3762a7871223e0fa67bbf8574ff7c11c9f89b80
Xinru Yun <54689291+YXR0529@users.noreply.github.com>
2019-12-06 13:42:04 -0500

    Add files via upload

--- Final_project_1_Standard_Sudoku.py
+++ Final_project_1_Standard_Sudoku.py
@@ -1,23 +1,24 @@
 '''
 '''
+import functools
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
 
 class standard_sudoku():
-
-
     def read_sdk(self, filename):
         '''
-    This is a function used to read the sudoku file as a dictionary.
-    (Written by Xinru)
+        This is a function used to read the sudoku file as a dictionary.
+        (Written by Xinru)
 
-    **Parameters**
+        **Parameters**
 
-        filename: *string*
+            filename: *string*
 
-    **Returns**
+        **Returns**
 
-        file: *dictionary*
-    '''
+            file: *dictionary*
+        '''
         f = open(filename, "r")
         file = {}
         number = []
@@ -38,7 +39,7 @@ class standard_sudoku():
                         lst.append(int(x))
                 number.append(lst)
         # Add number to the dictionary.
-        file["numbers"] = number
+        file["map"] = number
         # Skip the line before "MAP_START"
         for line in f:
             if line.strip() == "MAP_START":
@@ -54,31 +55,193 @@ class standard_sudoku():
                     if y != " ":
                         lst.append(y)
                 map.append(lst)
+        file['groups'] = {}
         for y, row in enumerate(map):
             for x, z in enumerate(row):
-                file[z] = []
+                file['groups'][z] = []
         for y, row in enumerate(map):
             for x, z in enumerate(row):
-                file[z].append((x + 1, y + 1))
+                file['groups'][z].append((x, y))
         # Close the file
         f.close()
         return file
 
-    def load_sdk_map(self):
-        pass
+    def load_sdk_map(self, info_dict):
+        '''
+        '''
+        # Initialize all blank positions on the map
+        info_dict['original_numbers'] = {}
+        info_dict['blank_positions'] = []
+        info_dict['fixed_number'] = {}
+        for row in range(len(info_dict['map'])):
+            for column in range(len(info_dict['map'][row])):
+                if info_dict['map'][row][column] == 0:
+                    info_dict['blank_positions'].append((column, row))
+                else:
+                    info_dict['original_numbers'][(column, row)] =\
+                        info_dict['map'][row][column]
+        info_dict['possibility'] = {}
+        for point in info_dict['blank_positions']:
+            info_dict['possibility'][point] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return info_dict
 
-    def sub_points(self):
-        pass
+    def sub_points(self, info_dict):
+        '''
+        '''
+        info_dict['fixed_number'].update(info_dict['original_numbers'])
+        for fp in info_dict['fixed_number']:
+            if fp in info_dict['possibility']:
+                info_dict['possibility'].pop(fp)
+            fp_x, fp_y = fp[0], fp[1]
+            # Sub Row Possibilities
+            test_x = len(info_dict['map'][0])
+            while test_x >= 0:
+                if (test_x, fp_y) in info_dict['possibility']:
+                    if info_dict['fixed_number'][fp] in\
+                            info_dict['possibility'][(test_x, fp_y)]:
+                        info_dict['possibility'][(test_x, fp_y)].remove(
+                            info_dict['fixed_number'][fp])
+                test_x -= 1
+            # Sub Column Possibilities
+            test_y = len(info_dict['map'])
+            while test_y >= 0:
+                if (fp_x, test_y) in info_dict['possibility']:
+                    if info_dict['fixed_number'][fp] in\
+                            info_dict['possibility'][(fp_x, test_y)]:
+                        info_dict['possibility'][(fp_x, test_y)].remove(
+                            info_dict['fixed_number'][fp])
+                test_y -= 1
+            # Sub Group Possibilities
+            for grp in info_dict['groups']:
+                if fp in info_dict['groups'][grp]:
+                    for point in info_dict['groups'][grp]:
+                        if point in info_dict['possibility']:
+                            if info_dict['fixed_number'][fp] in\
+                                    info_dict['possibility'][point]:
+                                info_dict['possibility'][point].remove(
+                                    info_dict['fixed_number'][fp])
+        info_dict['shortest'] = functools.reduce(
+            lambda x, y: x if len(
+                info_dict['possibility'][x]
+            ) < len(info_dict['possibility'][y])
+            else y, info_dict['possibility'])
+        return info_dict
 
-    def solve_sdk(self):
-        pass
+    def next_number(self, info_dict, next_list):
+        '''
+        '''
+        info_dict = standard_sudoku.sub_points(self, info_dict)
+        new_list = []
+        for p in next_list:
+            info_dict = standard_sudoku.load_sdk_map(self, info_dict)
+            # print(p)
+            info_dict['fixed_number'].update(p)
+            info_dict = standard_sudoku.sub_points(self, info_dict)
+            # print(info_dict['shortest'])
+            # print(info_dict['possibility'][info_dict['shortest']])
+            # print(info_dict)
+            if info_dict['shortest'] != {}:
+                for n in info_dict['possibility'][info_dict['shortest']]:
+                    possibility = {}
+                    for ps in p:
+                        possibility[ps] = p[ps]
+                    possibility[info_dict['shortest']] = n
+                    # possibility.update({info_dict['shortest']: n})
+                    new_list.append(possibility)
+                    # print(new_list)
+                # print(new_list)
+                info_dict['possibility'].pop(info_dict['shortest'])
+        return new_list
 
-    def show_solution(self):
-        pass
+    def solve_sdk(self, info_dict):
+        info_dict = standard_sudoku.sub_points(self, info_dict)
+        next_list = []
+        for n in info_dict['possibility'][info_dict['shortest']]:
+            next_list.append({info_dict['shortest']: n})
+        while info_dict['possibility'] != {}:
+            next_list = standard_sudoku.next_number(self, info_dict, next_list)
+            # print(next_list)
+            # print(len(next_list))
+        info_dict['final'] = {}
+        info_dict['final'].update(next_list[0])
+        return info_dict
+
+    def save_sdk_txt(self, info_dict, filename):
+        '''
+        '''
+        f = open(filename, 'w')
+        map = info_dict['map']
+        for (p_x, p_y) in info_dict['final']:
+            map[p_y][p_x] = info_dict['final'][(p_x, p_y)]
+        for y in range(len(map)):
+            for x in range(len(map[0])):
+                    f.write(str(map[y][x]) + ' ')
+            f.write('\n')
+        f.close()
+        return f
+
+    def set_color(self, img, x0, y0, dim, gap, color):
+
+        for x in range(dim):
+            for y in range(dim):
+                img.putpixel((dim * x0 + x + gap, dim * y0 + y + gap), color)
+
+    def save_sdk_img(self, info_dict, filename):
+        map = info_dict['map']
+        for (p_x, p_y) in info_dict['final']:
+            map[p_y][p_x] = info_dict['final'][(p_x, p_y)]
+
+        # Create a new image.
+        w_blocks = len(map[0])
+        h_blocks = len(map)
+        size = (w_blocks * 50 + 40, h_blocks * 50 + 40)
+
+        image = Image.new("RGB", size, color=(255, 255, 255))
+
+        for n in info_dict['groups']:
+            color = tuple(np.random.choice(range(200, 256), size=3))
+            for x, y in info_dict['groups'][n]:
+                dim = 50
+                gap = 20
+                standard_sudoku.set_color(self, image, x, y, dim, gap, color)
+
+        # Draw the grid.
+        draw = ImageDraw.Draw(image)
+        y_i = 20
+        y_f = image.height - 20
+        step_size_x = int((image.width - 40) / len(map[0]))
+        step_size_y = int((image.height - 40) / len(map))
+
+        for x in range(20, image.width, step_size_x):
+            line = ((x, y_i), (x, y_f))
+            draw.line(line, 'black')
+
+        x_i = 20
+        x_f = image.width - 20
+
+        for y in range(20, image.height, step_size_y):
+            line = ((x_i, y), (x_f, y))
+            draw.line(line, fill=128)
+
+        # Draw the numbers.
+        font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
+        for y, lst in enumerate(map):
+            for x, i, in enumerate(map[y]):
+                w, h = draw.textsize(str(i))
+                draw.text(
+                    (20 + (50 - w) / 2 + x * 50, 20 + (50 - h) / 2 + y * 50),
+                    str(i), 'black', font=font
+                )
+        image.save(filename)
 
 
 if __name__ == "__main__":
     a = standard_sudoku()
     b = a.read_sdk("standard_hard_8_0.sdk")
-    print(b)
-
+    c = a.load_sdk_map(b)
+    print(c)
+    d = a.sub_points(c)
+    e = a.solve_sdk(c)
+    print(e)
+    a.save_sdk_txt(e, "standart_hard_8_0.txt")
+    a.save_sdk_img(e, "standart_hard_8_0.png")

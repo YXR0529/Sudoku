c08387b75ab71e17412058db8ba7e23267d04247
PJMantis <54689263+PJMantis@users.noreply.github.com>
2019-12-06 23:16:38 -0500

    Add files via upload
    completed comments of standard sudoku

--- Final_project_1_Standard_Sudoku.py
+++ Final_project_1_Standard_Sudoku.py
@@ -1,4 +1,56 @@
 '''
+Project: Sudoku
+Authors: Xinru Yun, Tianjun Tang(Don)
+
+Course: Software Carpentry
+Johns Hopkins University
+
+
+This is our Final Project of Software Carpentry,
+in which we decided to apply BFS (Breadth First Search)
+method to solve various kinds of sudoku games.
+
+Our data source is Conceptis, which is a series of games
+that can be downloaded from Appstore, and contains six kinds
+of Sudoku to play with, that are, standard, curve, diagonal,
+odd/even, multi and killer sudoku.
+
+Based on standard sudoku rules, they added different extra rules
+like, in odd/even, some places can only be odd or even numbers, or,
+in killer sudoku, blocks have been devided into even smaller groups
+while requiring their sum to be a given number.
+
+Generally, to solve a sudoku, you need a .sdk file, which
+basically looks like following:
+
+TYPE: STANDARD_SUDOKU # indicates type of sudoku
+
+NUMBER_START # shows the numbers on the map, 0 if it's blank
+502090100
+000100080
+300006002
+040000700
+600000001
+005000090
+900700004
+060003000
+007020503
+NUMBER_END
+
+MAP_START # shows how numbers are divided into different groups
+a a a b b b c c c # in each group numbers should all be different
+a a a b b b c c c
+a a a b b b c c c
+d d d e e e f f f
+d d d e e e f f f
+d d d e e e f f f
+g g g h h h i i i
+g g g h h h i i i
+g g g h h h i i i
+MAP_END
+
+Further nomenclatures can be found in our repository under main folder.
+
 '''
 import functools
 
@@ -66,11 +118,38 @@ class standard_sudoku():
 
     def load_sdk_map(self, info_dict):
         '''
+        This part is written by Don.
+
+        This function reads raw informations info_dict after read_sdk()
+        and convert them into more useful and processable forms, which
+        contains:
+
+        *** groups ***
+        Shows which groups that each point belongs to.
+
+        *** original numbers ***
+        The Given numbers on the map, which will be merged
+        with manually putted points during solution process.
+
+        *** Blank Positions ***
+        A list showing all blank positions on the map, only used
+        in this function.
+
+        *** Fixed Number ***
+        An empty dictionary to store manually putted points during
+        solution.
+
+        *** Possibility ***
+        A dictionary showing which numbers can be putted in a given
+        position, this part is the key to solve problems.
+
+
         '''
-        # Initialize all blank positions on the map
+        # Initialize some informations in the info_dict
         info_dict['original_numbers'] = {}
         info_dict['blank_positions'] = []
         info_dict['fixed_number'] = {}
+        # Input infos about both given points and blank positions
         for row in range(len(info_dict['map'])):
             for column in range(len(info_dict['map'][row])):
                 if info_dict['map'][row][column] == 0:
@@ -78,6 +157,7 @@ class standard_sudoku():
                 else:
                     info_dict['original_numbers'][(column, row)] =\
                         info_dict['map'][row][column]
+        # Generate a dictionary of possibility of all blank points
         info_dict['possibility'] = {}
         for point in info_dict['blank_positions']:
             info_dict['possibility'][point] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -85,6 +165,18 @@ class standard_sudoku():
 
     def sub_points(self, info_dict):
         '''
+        This part is written by Don.
+
+        This function removes those impossible 'possibilities' of each
+        position, whose rules varies and gets increasingly complicated when
+        solving more advanced problems.
+
+        Following code contains three parts:
+        # remove im_possibilities at each row/column
+        # remove im_possibilities within each group
+        # generate a position whose possibility list is shortest, which
+            ## can be used for BFS growth
+            ## shows wrong combination when it's []
         '''
         info_dict['fixed_number'].update(info_dict['original_numbers'])
         for fp in info_dict['fixed_number']:
@@ -118,6 +210,7 @@ class standard_sudoku():
                                     info_dict['possibility'][point]:
                                 info_dict['possibility'][point].remove(
                                     info_dict['fixed_number'][fp])
+        # Get shortest possibility
         info_dict['shortest'] = functools.reduce(
             lambda x, y: x if len(
                 info_dict['possibility'][x]
@@ -127,52 +220,60 @@ class standard_sudoku():
 
     def next_number(self, info_dict, next_list):
         '''
+        This part is written by Don.
+
+        This functions is for BFS growth.
+
+        Given current list of combinations, which is stored as a list
+        of dictionaries, this function initialize the map and make point
+        subtractions, and judges whether the combination is valid.
+
+        If the shortest possibilities is not [], then duplicate current
+        combinations and paste different possibility in the end, and
+        return a new list of right combinations.
         '''
         info_dict = standard_sudoku.sub_points(self, info_dict)
         new_list = []
         for p in next_list:
             info_dict = standard_sudoku.load_sdk_map(self, info_dict)
-            # print(p)
             info_dict['fixed_number'].update(p)
             info_dict = standard_sudoku.sub_points(self, info_dict)
-            # print(info_dict['shortest'])
-            # print(info_dict['possibility'][info_dict['shortest']])
-            # print(info_dict)
             if info_dict['shortest'] != {}:
                 for n in info_dict['possibility'][info_dict['shortest']]:
                     possibility = {}
                     for ps in p:
                         possibility[ps] = p[ps]
                     possibility[info_dict['shortest']] = n
-                    # possibility.update({info_dict['shortest']: n})
                     new_list.append(possibility)
-                    # print(new_list)
-                # print(new_list)
                 info_dict['possibility'].pop(info_dict['shortest'])
         return new_list
 
     def solve_sdk(self, info_dict):
+        '''
+        This part is written by Don.
+
+        This functions are all the same in different files.
+        By subtracting points first, we get an initial possible
+        combination to start with.
+
+        Repeat BFS generation until the possibility dictionary
+        is empty, and returns the final answer.
+        '''
         info_dict = standard_sudoku.sub_points(self, info_dict)
         next_list = []
         for n in info_dict['possibility'][info_dict['shortest']]:
             next_list.append({info_dict['shortest']: n})
         while info_dict['possibility'] != {}:
             next_list = standard_sudoku.next_number(self, info_dict, next_list)
-            # print(next_list)
-            # print(len(next_list))
         info_dict['final'] = {}
         info_dict['final'].update(next_list[0])
         return info_dict
 
-    def show_solution(self):
-        pass
-
 
 if __name__ == "__main__":
     a = standard_sudoku()
-    b = a.read_sdk("standard_master_100_0.sdk")
+    b = a.read_sdk("standard_hard_world.sdk")
     c = a.load_sdk_map(b)
-    print(c)
     d = a.sub_points(c)
     e = a.solve_sdk(c)
     print(e)
